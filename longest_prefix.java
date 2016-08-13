74494e8143b8f21793c00101861da1621dbd9784
Sujith-V <sujithborn4u@gmail.com>
2016-08-13 15:31:53 +0530

    Update longest_prefix.java

--- longest_prefix.java
+++ longest_prefix.java
@@ -5,7 +5,7 @@ class longest_prefix
   {
     Scanner sc=new Scanner(System.in);
     String s="",str[]=sc.nextLine().split(" ");
-    int count=0,max=-1;
+    int count=0;
     for(int i=0;i<str.length-1;i++)
     {
       for(int j=i+1;j<str.length;j++)
@@ -16,15 +16,12 @@ class longest_prefix
             count++;
           else
             break;
-        if(count>max)
-        {
-          max=count;
+        if(count>s.length())
           s=str[i].substring(0,count);
-        }
       }
     }
     if(s=="")
-    System.out.println(null);
+    System.out.println("null");
     else
     System.out.println(s);
   }
