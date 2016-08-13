1f3e05caf0ca081d4815b805478b282acb0281f9
Sujith-V <sujithborn4u@gmail.com>
2016-08-13 14:40:29 +0530

    Update least_num.java

--- least_num.java
+++ least_num.java
@@ -1,11 +1,12 @@
-
+import java.util.Scanner;
+import java.util.ArrayList;
 class least_num
 {
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 String s=sc.next();
-ArraList<Integer> list=new ArrayList<Integer>();
+ArrayList<Integer> list=new ArrayList<Integer>();
 int i=0,k=sc.nextInt();
 if(s.charAt(0)=='-')
   i=1;
@@ -17,7 +18,7 @@ if(s.charAt(0)=='-')
   Collections.reverse(list);
   System.out.print('-');
 }
-for(i=0;i<set.size()-k;i++)
+for(i=0;i<list.size()-k;i++)
 System.out.print(list.get(i));
 }
 }
