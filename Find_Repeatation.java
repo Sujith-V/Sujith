146455a7a9786c95d05fc0eecebc20f85db6996e
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 10:08:08 +0530

    Update Find_Repeatation.java

--- Find_Repeatation.java
+++ Find_Repeatation.java
@@ -8,6 +8,7 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 ArrayList<Integer> list=new ArrayList<Integer>();
 LinkedHashSet<Integer> set=new LinkedHashSet();
+int n=sc.nextInt();
 for(int i=0;i<n;i++)
 {
 int x=sc.nextInt();
