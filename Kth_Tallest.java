a004831b41e8af8b11f735207d3236c721ed9608
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 14:08:30 +0530

    Update Kth_Tallest.java

--- Kth_Tallest.java
+++ Kth_Tallest.java
@@ -8,6 +8,11 @@ class Kth_Tallest
     for(int i=0;i<50;i++)
     arr[i]=sc.nextInt();
     int k=sc.nextInt();
+    if(k>50)
+    {
+      System.out.println("There is "+k+" Students in given list");
+      return;
+    }
     System.out.println("4th Tallest : "+arr[arr.length-4]+"\n"+k+"th Tallest : "+arr[arr.length-k]);
   }
 }
