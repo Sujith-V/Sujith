06f69306bfce38b794ca28da4cabc0ea6ce69280
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 12:41:50 +0530

    Update Unique_Integer2.java

--- Unique_Integer2.java
+++ Unique_Integer2.java
@@ -1,2 +1,23 @@
 import java.util.*;
-class
+class Unique_Integer2
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner(System.in);
+  int x=0,n=sc.nextInt();
+  LinkedHashSet<Integer> set=new LinkedHashSet<Integer>();
+  for(int i=0;i<n;i++)
+  {
+    x=sc.nextInt();
+    if(!set.add(x))
+    set.remove(x);
+  }
+  Iterator<Integer> itr=set.iterator();
+  if(set.size()==0)
+  {
+    System.out.println("No numbers were found");
+    return;
+  }
+  System.out.println(itr.next());
+  }
+}
