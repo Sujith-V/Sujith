ada00b3138c7317eabe40a82c72d281b8a409939
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 13:18:09 +0530

    Update Close_To_Zero.java

--- Close_To_Zero.java
+++ Close_To_Zero.java
@@ -1,1 +1,22 @@
-
+import java.util.*;
+class Close_To_Zero
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int x=0,y=0,n=sc.nextInt(),arr[]=new int[n];
+    for(int i=0;i<n;i++)
+    arr[i]=sc.nextInt();
+    Arrays.sort(arr);
+    int diff=arr[arr.length-1];
+    for(int i=0;i<n-1;i++)
+    for(int j=i+1;j<n;j++)
+    if(Math.abs(arr[i]+arr[j]<diff)
+    {
+      x=arr[i];
+      y=arr[j];
+      diff=Math.abs(x+y);
+    }
+    System.out.println(x+" "+y);
+  }
+}
