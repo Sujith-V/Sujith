40a297313481721d7706c8fad362b367d38c2bb3
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 13:49:26 +0530

    Update Reverse_Order_Of_String.java

--- Reverse_Order_Of_String.java
+++ Reverse_Order_Of_String.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class Reverse_Order_Of_String
+{
+  static String Reverse_Order(String str[])
+  {
+    String s="";
+    for(int i=str.length-1;i>=0;i--)
+    s+=str[i]+" ";
+    return s;
+  }
+  public static void main(String args[])
+  {
+    System.out.println(Reverse_Order(new Scanner(System.in).nextLine().split(" ")));
+  }
+}
