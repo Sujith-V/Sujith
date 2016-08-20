bf030e82fccdfd7a16959b9c78945d6c087366c1
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 13:31:34 +0530

    Update Array_Subset.java

--- Array_Subset.java
+++ Array_Subset.java
@@ -1,1 +1,18 @@
 
+import java.util.*;
+class Array_Subset
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    ArrayList<Integer> list1=new ArrayList<Integer>();
+    ArrayList<Integer> list2=new ArrayList<Integer>();
+    for(int i=0;i<n;i++)
+    list1.add(sc.nextInt());
+    n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    list2.add(sc.nextInt());
+    System.out.println(list1.containsAll(list2));
+  }
+}
