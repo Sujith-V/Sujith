f57250921162e40f83e1539540161902730d374b
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 12:30:56 +0530

    Update First_Repeated_Number.java

--- First_Repeated_Number.java
+++ First_Repeated_Number.java
@@ -1,1 +1,21 @@
-
+import java.util.ArraList;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+class First_Repeated_Number
+{
+  Scanner sc=new Scanner(System.in);
+  int x,n=sc.nextInt();
+  LinkedHashSet<Integer> set=new LinkedHashSet<Integer>();
+  for(int i=0;i<n;i++)
+  {
+    x=sc.nextInt();
+    if(!set.add(x))
+    break;
+  }
+  if(set.size()==0)
+  {
+    System.out.println("No numbers were found");
+    return;
+  }
+  System.out.println(x);
+}
