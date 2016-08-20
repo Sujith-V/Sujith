9cd0abdee60a540357825564283bb07534183507
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 13:03:54 +0530

    Update I_plus_J_equals_K.java

--- I_plus_J_equals_K.java
+++ I_plus_J_equals_K.java
@@ -5,12 +5,14 @@ class I_plus_J_equals_K
   {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt(),arr[]=new int[n];
+    String s="";
     for(int i=0;i<n;i++)
     arr[i]=sc.nextInt();
     for(int i=0;i<n;i++)
     for(int j=0;j<i;j++)
     for(int k=i;k<n;k++)
     if(arr[i]+arr[j]==arr[k])
-    System.out.println(arr[j]+" + "+arr[i]+" = "+arr[k]);
+    s=s+arr[j]+" + "+arr[i]+" = "+arr[k]+"\n";
+    System.out.println((s=="")?"No numbers were found":s);
   }
 }
