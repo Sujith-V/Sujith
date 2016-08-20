e59ebd4dc99d801e939de379d249b93b39258169
Sujith-V <sujithborn4u@gmail.com>
2016-08-20 14:06:28 +0530

    Update Kth_Tallest.java

--- Kth_Tallest.java
+++ Kth_Tallest.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+class Kth_Tallest
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int arr[]=new int[50];
+    for(int i=0;i<50;i++)
+    arr[i]=sc.nextInt();
+    int k=sc.nextInt();
+    System.out.println("4th Tallest : "+arr[arr.length-4]+"\n"+k+"th Tallest : "+arr[arr.length-k]);
+  }
+}
