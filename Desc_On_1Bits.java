aae8c7f04a05c4de83e96c11853d1e1d2a66d96c
Sujith-V <sujithborn4u@gmail.com>
2016-08-22 19:23:17 +0530

    Update Desc_On_1Bits.java

--- Desc_On_1Bits.java
+++ Desc_On_1Bits.java
@@ -1,1 +1,44 @@
-
+import java.util.*;
+class Desc_On_1Bits.java
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    ArrayList<Integer> nums=new ArrayList<Integer>(),counts=new ArrayList<Integer>();
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    {
+      int x=sc.nextInt();
+      String s=Integer.tobinaryString(x);
+      for(int j=0;j<s.length(0);j++)
+      if(s.charAt(j)=='1')
+      count++;
+      nums.add(x);counts.add(count);
+    }
+    int len=counts.size();
+    for(int i=0;i<len;i++)
+    {
+      int max=-1,max_num=nums.get(0),index=-1;
+      for(int j=0;j<counts.aixe();j++)
+      {
+        if(counts.get(j)>max)
+        {
+          max=counts.get(j);
+          max_num=nums.get(j);
+          index=j;
+        }
+        else if(counts.get(j)==max)
+        {
+          if(nums.get(j)>max)
+          {
+            max_num=nums.get(j);
+            index=j;
+          }
+        }
+      }
+      System.out.println(max_num);
+      nums.remove(index);
+      counts.remove(index);
+      }
+    }
+  }
