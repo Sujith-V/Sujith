ff47f788560126094b331c1d25eab37e4efa33e8
Sujith-V <sujithborn4u@gmail.com>
2016-08-23 18:34:58 +0530

    Update Seed.java

--- Seed.java
+++ Seed.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+class Seed
+{
+  public static void main(String args[])
+  {
+    int n=new Scanner(System.in).nextInt();
+    for(int i=1;i<=n;i++)
+    {
+      String s=i+"";
+      int t=1;
+      for(int j=0;j<s.length();j++)
+      t*=Integer.parseInt(s.charAt(j)+"");
+      if(i*t==n)
+      System.out.println(i);
+    }
+  }
+}
