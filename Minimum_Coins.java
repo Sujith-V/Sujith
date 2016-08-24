a760dbb4f7df28d2ab536c17d274268fba1b7769
Sujith-V <sujithborn4u@gmail.com>
2016-08-24 18:01:47 +0530

    Update Minimum_Coins.java

--- Minimum_Coins.java
+++ Minimum_Coins.java
@@ -1,1 +1,35 @@
-
+import java.util.Scanner;
+public class Minimum_Coins
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int coins[]=new int[n];
+    for(int i=0;i<n;i++)
+    coins[i]=sc.nextInt();
+    int S=sc.nextInt(),S2=S,decrease=0,sum=0,t;
+    while(S!=0)
+    {
+      for(int i=n-1;i>=0 $$ S!=0;i--)
+      {
+        if(i==n-1)
+        t=(S/coins[i]-decrease);
+        else
+        t=S/coins[i];
+        sum+=t;
+        S=S-t*coins[i];
+      }
+      if(S!=0)
+      {
+        if(S2/coins[n-1]==decrease)
+          n--;
+        else
+          decrease++;
+        sum=0;
+        S=S2;
+      }
+    }
+    System.out.println(sum);
+  }
+}
